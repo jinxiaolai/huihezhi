a028a1c3b05c841d99f5f9bbb4d2a12f3b749555
jinxiaolai <o8542794851@163.com>
2019-11-21 16:40:58 +0800

    回合制手游提交

---
+++ Assets/C/camera.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public Transform player;
+    Vector3 pos;
+
+    void Update()
+    {
+
+        if (player.position.y <= -33)
+        {
+            transform.position = new Vector3(player.position.x, -33, -10);
+            if (player.position.x <= -422)
+                transform.position = new Vector3(-422, -33, -10);
+            if (player.position.x >= 872)
+                transform.position = new Vector3(872, -33, -10);
+        }
+
+        if (player.position.y >= 254)
+        {
+            transform.position = new Vector3(player.position.x, 254, -10);
+            if (player.position.x <= -422)
+                transform.position = new Vector3(-422, 254, -10);
+            if (player.position.x >= 872)
+                transform.position = new Vector3(872, 254, -10);
+        }
+        if (player.position.x <= -422)
+        {
+            transform.position = new Vector3(-422, player.position.y, -10);
+            if (player.position.y <= -33)
+                transform.position = new Vector3(-422, -33, -10);
+            if (player.position.y >= 254)
+                transform.position = new Vector3(-422, 254, -10);
+        }
+        if (player.position.x >= 872)
+        {
+            transform.position = new Vector3(872, player.position.y, -10);
+            if (player.position.y <= -33)
+                transform.position = new Vector3(872, -33, -10);
+            if (player.position.y >= 254)
+                transform.position = new Vector3(872, 254, -10);
+        }
+
+
+
+    }
+
+}
